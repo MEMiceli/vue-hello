6a599dca7c947004b4aea5f6ea8e553e23df933b
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-05-20 10:11:14 +0200

    doveva funzionare già da ieri..

--- js/main.js
+++ js/main.js
@@ -6,9 +6,15 @@
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
+
+
 let app = new Vue( {
         el: '#root',
         data:{
-            imprecazione: 'sin da ciappu ri struppiu'
-        }
-})
+            imprecazione: 'sin da ciappu ri struppiu',
+            img:'https://i.picsum.photos/id/1027/2848/4272.jpg?hmac=EAR-f6uEqI1iZJjB6-NzoZTnmaX0oI0th3z8Y78UpKM',
+        },
+
+});
+
+
